17bdbebfa140ba04ed6e4717d0ec561d53ae4c1e
Luca Ruiters <lucaruiters@hotmail.nl>
2021-04-09 22:17:49 +0200

    Initial Commit

---
+++ app/src/main/java/tech/lucar/formalelementsrandomizer/ui/history/HistoryFragment.kt
@@ -0,0 +1,31 @@
+package tech.lucar.formalelementsrandomizer.ui.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import tech.lucar.formalelementsrandomizer.R
+
+class HistoryFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: HistoryViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+                ViewModelProvider(this).get(HistoryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_history, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
